a93d5682a97b7226434c58edd0f198f51b2d649a
MelissaMarquez3 <135534484+MelissaMarquez3@users.noreply.github.com>
2023-06-20 08:46:01 -0500

    Homework 4

---
+++ CIDM Homework4 Q1&2 Done.cs
@@ -0,0 +1,69 @@
+﻿namespace Homework4Q1;
+
+
+
+//Q1  method in Main()
+public class Largest1
+{
+    public static int largest(int num1, int num2)
+{
+    if (num1 > num2)
+{
+        return num1;
+}
+else
+{
+        return num2;
+    }
+}
+    static void Main(string[] args)
+    {
+        Console.Write("a= ");
+        int first = Convert.ToInt32(Console.ReadLine());
+        Console.Write("b= ");
+        int second = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The largest number is: " + largest(first, second));
+        
+    }
+}
+
+
+//Q2 method in Main()
+public class Largest2
+{
+    public static int largest(int num1, int num2)
+    {
+        if (num1 > num2)
+        {
+            return num1;
+        }
+        else
+        {
+            return num2;
+        }
+}
+public static int largest(int num1,int num2,int num3, int num4)
+{
+    return largest(largest(num1,num2), largest(num3, num4));
+}
+public  void Main(string[] args)
+{
+        Console.Write("a= ");
+        int first = Convert.ToInt32(Console.ReadLine());
+        Console.Write("b= ");
+        int second = Convert.ToInt32(Console.ReadLine());
+        Console.Write("c= ");
+        int third = Convert.ToInt32(Console.ReadLine());
+        Console.Write("d= ");
+        int fourth = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The largest number is: " + largest(first, second, third, fourth));
+    }
+}    
+
+//Q3
+//create account method ()
+
+
+
+
+
---
+++ CIDM Homework4 Q3 DONE (1).cs
@@ -0,0 +1,60 @@
+﻿namespace Homework4Q3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // createAccount () method 
+        createAccount();
+        Console.ReadKey(true);
+    }
+    public static bool checkAge(int birth_year)
+{
+    //calculate age
+    int age=2022-birth_year;
+    //if age is greater than or equal to 18, then return true
+    if(age>=18)
+        return true;
+    //otherwise return false
+    else
+        return false;
+}
+public static void createAccount()
+{
+    //declare string variables userName, password and retypedPassword
+    String userName,password,retypedPassword;
+    //declare interger variable birthyear
+    int birthYear;
+    //prompt user to enter user name
+    Console.WriteLine("Enter Your Username:");
+    //read userName
+    userName=Console.ReadLine();
+    //prompt user to enter password
+    Console.WriteLine("Enter Your Password:");
+    //read password
+    password=Console.ReadLine();
+    //prompt user to enter password again
+    Console.WriteLine("Enter Your Password Again:");
+    //read retypedPassword
+    retypedPassword=Console.ReadLine();
+    //prompt user to enter birth year
+    Console.WriteLine("Enter Your Birthyear:");
+    //read birthYear
+    birthYear=Convert.ToInt32(Console.ReadLine());
+    //if checkAge() return true, then
+    if (checkAge(birthYear))
+    {
+        //if password and retypedPassword are equal, then displays
+        //"Account is created successfully"
+        if (password==retypedPassword)
+            Console.WriteLine("Account is created successfully");
+            //otherwise display "Wrong password"
+            else
+                Console.WriteLine("Wrong password");
+    }
+    //otherwise display "Could not create an account."
+    else
+        Console.WriteLine("Could not create an account.");
+    }
+
+}
