ceb0243a07d7fea87aa434c2ec8fb6d6267f89c7
MelissaMarquez3 <135534484+MelissaMarquez3@users.noreply.github.com>
2023-07-11 12:48:19 -0500

    CIDM Midterm Bonus Question

---
+++ CIDM BonusQuestion.cs
@@ -0,0 +1,51 @@
+﻿namespace BonusExamQuestion;
+public void PlayRound()
+{
+// Get the choice of each player
+string choice1 = player1.HumanDecision();
+string choice2 = player2.ComputerDecision();
+
+// Determine the winner based on the choices
+string winner = DetermineWinner(choice1, choice2);
+
+// Update the points of the players
+if (winner == "player1")
+{
+player1.WinRound();
+player2.LoseRound();
+Console.WriteLine("You win the round!");
+}
+else if (winner == "player2")
+{
+player2.WinRound();
+player1.LoseRound();
+Console.WriteLine("You lose the round!");
+}
+else
+{
+Console.WriteLine("It's a tie!");
+}
+
+// Print the points of human player
+Console.WriteLine("Your points: " + player1.GetPoints());
+
+// Check if the game should continue
+if (player1.GetPoints() <= 0)
+{
+Console.WriteLine("Sorry, you don't have enough points. Thanks for playing!");
+return;
+}
+
+// Ask the player to continue or exit
+Console.WriteLine("Play again? Input 'y' to continue, or 'n' to exit");
+string input = Console.ReadLine();
+
+if (input.ToLower() == "n")
+{
+    Console.WriteLine("Thank you for playing!");
+return;
+}
+
+// Clear the console for the next round
+Console.Clear();
+}
